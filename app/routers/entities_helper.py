8e84d7451a199697dcf6dd374ff0827bb69d1d16
cjwbrandon <cjwbrandon@gmail.com>
2021-03-14 11:49:43 +0800

    [ADD] Helper function to do a simple Entity Extraction using Spacy

--- app/routers/entities_helper.py
+++ app/routers/entities_helper.py
@@ -2,8 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
+import spacy
+import en_core_web_sm
+import pandas as pd
 
 
+# Extract
+#########
 def extract_text_body(url, retries=3):
     """Extract text body of a url
     
@@ -39,3 +44,35 @@ def extract_text_body(url, retries=3):
         script.extract()
 
     return " ".join(re.split(r"[\n\t\s]+", soup.get_text()))
+
+
+# Transform
+###########
+def extract_entities_w_spacy(text):
+    """Extract entities from input text using Spacy.
+    
+    Parameters
+    ----------
+    text : string
+        text to extract from.
+        
+    Returns
+    -------
+    pandas.DataFrame
+        Table of the texts and their entities.
+        
+    Raises
+    ------
+    AssertionException
+        Input text must be of type string
+    AssertionException
+        Input text cannot be empty
+    """
+    assert type(text) == str, "Input text must be of type string."
+    assert text != "", "Input text cannot be empty."
+
+    nlp = en_core_web_sm.load()
+    doc = nlp(text)
+    return pd.DataFrame(
+        [(X.text, X.label_) for X in doc.ents], columns=["text", "entity"]
+    )
